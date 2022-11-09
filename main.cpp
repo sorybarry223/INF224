50003438ba20685ef61e459503520a9b83145742
Sory Ibrahima Barry <sorybarry@Sorys-MacBook-Air.local>
2022-11-09 18:04:29 +0100

    Etape N1

--- main.cpp
+++ main.cpp
@@ -8,6 +8,6 @@ using namespace std;
 
 int main(int argc, const char* argv[])
 {
-    std::cout << "Hello brave new world" << std::endl;
+    std::cout << " Hello brave new world" << std::endl;
     return 0;
 }
