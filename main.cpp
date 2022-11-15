a509bd6a3e6ae6af7bebb9bc26ab4b084fdeec28
Sory Ibrahima Barry <sorybarry@Sorys-MacBook-Air.local>
2022-11-15 20:06:01 +0100

    Etape 3

--- main.cpp
+++ main.cpp
@@ -6,11 +6,12 @@
 #include <iostream>
 #include <stdlib.h>
 #include <string.h>
-
+#include "multimedia.h"
 using namespace std;
 
 int main(int argc, const char *argv[])
 {
-    std::cout << "Hello brave new world" << std::endl;
-    return 0;
+    Multimedia * m=new Multimedia("photo1","/usr/bin/local");
+    m->affiche(std::cout);
+    return 0; 
 }
