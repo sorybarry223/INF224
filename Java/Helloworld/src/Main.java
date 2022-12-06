2bdcfd39b617fe92731d32dafca2b2a503b82c2b
Sory Ibrahima Barry <sorybarry@Sorys-MacBook-Air.local>
2022-12-06 17:52:50 +0100

    buttons

--- Java/Helloworld/src/Main.java
+++ Java/Helloworld/src/Main.java
@@ -11,36 +11,6 @@ import javax.swing.ImageIcon;
 public class Main {
     public static void main(String[] args) {
       
-      ImageIcon icon =new ImageIcon("logo.png");
-
-      JLabel label= new JLabel();
-      label.setText("Hello world");
-      label.setIcon(icon); 
-     // label.setVerticalAlignment(JLabel.TOP);
-     // label.setHorizontalAlignment(JLabel.RIGHT);
-      label.setBounds(100,100,75,75);
-
-      JPanel redpanel =new JPanel();
-      redpanel.setBackground(Color.red);
-      redpanel.setBounds(0,0,250,250);
-      redpanel.setLayout(null);
-      JPanel bluepanel =new JPanel();
-      bluepanel.setBackground(Color.blue);
-      bluepanel.setBounds(250,0,250,250);
-      
-      JPanel greenpanel =new JPanel();
-      greenpanel.setBackground(Color.green);
-      greenpanel.setBounds(0,250,500,250);
-      //greenpanel.setLayout(new BorderLayout());
-
-      JFrame frame=new JFrame();//creates a frame
-      frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //closes the frame
-      frame.setSize(720,720);//Sets the x-dimension and y-dimension of frame
-      frame.setLayout(null);
-      frame.setVisible(true);//Make frame visible
-      redpanel.add(label);
-      frame.add(redpanel);
-      frame.add(bluepanel);
-      frame.add(greenpanel);
+      new Myframe();
     }
 }
--- Java/Helloworld/src/Myframe.java
+++ Java/Helloworld/src/Myframe.java
@@ -1,19 +1,54 @@
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JFrame;
 import java.awt.Color;
+import java.awt.Font;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
+import javax.swing.BorderFactory;
 import javax.swing.ImageIcon;
-public class Myframe extends JFrame{
+import javax.swing.JButton;
+public class Myframe extends JFrame implements ActionListener{
+
+    JButton button=new JButton();
+    JLabel label;
     Myframe(){
        
-       /* this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //closes the frame
-        //frame.setResizable(false);//prevent frame from being resized
-        this.setSize(420,420);//Sets the x-dimension and y-dimension of frame
-        this.setVisible(true);//Make frame visible
-        ImageIcon image =new ImageIcon("logo-2.png"); //create an image icon
-        this.setIconImage(image.getImage());//change icon of frame
-       // frame.getContentPane().setBackground(Color.green);//change background color
-       this.getContentPane().setBackground(new Color(123,50,28));
-        */
+        ImageIcon icon=new ImageIcon("logo.png");
+        
+        label=new JLabel();
+        label.setIcon(icon);
+        label.setBounds(150,250,150,150);
+        label.setVisible(false);
+
+        button.setBounds(200,100,250,100);
+        button.addActionListener(this);
+        button.setText("Clik me");
+        button.setFocusable(false);
+       // button.setIcon(icon);
+        button.setHorizontalTextPosition(JButton.CENTER);
+        button.setVerticalTextPosition(JButton.BOTTOM);
+        button.setFont(new Font("Comic Sans", Font.BOLD,50));
+        button.setIconTextGap(-15);
+        button.setForeground(Color.cyan);
+        button.setBackground(Color.black);
+        button.setBorder(BorderFactory.createEtchedBorder());
+
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //closes the frame
+        this.setLayout(null);
+        this.setSize(500,500);//Sets the x-dimension and y-dimension of frame
+        this.setVisible(true);//Make frame visible 
+        this.add(button);
+        this.add(label);
+
     }
+    public void actionPerformed(ActionEvent e){
+        if(e.getSource()==button){
+            //System.out.println("poo");
+           // button.setEnabled(false);
+           label.setVisible(true);
+        }
+    }
+     
 }
